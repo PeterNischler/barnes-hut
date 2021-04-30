900ce52a28e666279a1a7a709f05a39ceea154fd
Muhammad Adam Ferdin <e11910541@student.tuwien.ac.at>
2021-04-30 17:30:17 +0200

    Octree

--- src/Octree.java
+++ src/Octree.java
@@ -1,4 +1,7 @@
 public class Octree {
     private Octree head;
-
+    private OctreeNode firstNode;
+    private OctreeNode secondNode;
+    private OctreeNode thirdNode;
+    private OctreeNode fourthNode;
 }
