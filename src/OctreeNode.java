790690c37937669291ad368b1e384f2c668e7fde
Peter Nischler <e12024711@student.tuwien.ac.at>
2021-05-05 22:50:01 +0200

    octreenode subnode center calculation

--- src/OctreeNode.java
+++ src/OctreeNode.java
@@ -1,16 +1,20 @@
 public class OctreeNode implements CosmicComponent {
     private Body body;
-    private CosmicComponent firstNode;
-    private CosmicComponent secondNode;
-    private CosmicComponent thirdNode;
-    private CosmicComponent fourthNode;
-    private CosmicComponent fifthNode;
-    private CosmicComponent sixthNode;
-    private CosmicComponent seventhNode;
-    private CosmicComponent eigthNode;
-    private Vector3 position; //center of octant
-    private Vector3 centreOfMass;
+    private Vector3 centre;
     private double diameter; //diameter of octant = edge length
+    //ungerade bei x minus, gerade x immer plus
+    //1-4 bei y immer plus, 5-8 bei y immer minus
+    //1,2,5,6 bei z plus, 3,4,7,8 bei z minus
+    private Vector3[] centreNodes = new Vector3[]{        new Vector3(centre.getX()-diameter/4, centre.getY()+diameter/4, centre.getZ()+diameter/4),
+            new Vector3(centre.getX()+diameter/4, centre.getY()+diameter/4, centre.getZ()+diameter/4),
+            new Vector3(centre.getX()-diameter/4, centre.getY()+diameter/4, centre.getZ()-diameter/4),
+            new Vector3(centre.getX()+diameter/4, centre.getY()+diameter/4, centre.getZ()-diameter/4),
+            new Vector3(centre.getX()-diameter/4, centre.getY()-diameter/4, centre.getZ()+diameter/4),
+            new Vector3(centre.getX()+diameter/4, centre.getY()-diameter/4, centre.getZ()+diameter/4),
+            new Vector3(centre.getX()-diameter/4, centre.getY()-diameter/4, centre.getZ()-diameter/4),
+            new Vector3(centre.getX()+diameter/4, centre.getY()-diameter/4, centre.getZ()-diameter/4),};
+    private CosmicComponent[] Nodes = new CosmicComponent[8];
+    private Vector3 centreOfMass;
 
     public OctreeNode(double d, Vector3 p) {
         diameter = d;
@@ -22,17 +26,7 @@ public class OctreeNode implements CosmicComponent {
         double positionX = body.getMassCenter().getX();
         double positionY = body.getMassCenter().getY();
         double positionZ = body.getMassCenter().getZ();
-        //ungerade bei x minus, gerade x immer plus
-        //1-4 bei y immer plus, 5-8 bei y immer minus
-        //1,2,5,6 bei z plus, 3,4,7,8 bei z minus
-        Vector3 firstNodeCentre = new Vector3(position.getX()-diameter/4, position.getY()+diameter/4, position.getZ()+diameter/4);
-        Vector3 secondNodeCentre = new Vector3(position.getX()+diameter/4, position.getY()+diameter/4, position.getZ()+diameter/4);
-        Vector3 thirdNodeCentre = new Vector3(position.getX()-diameter/4, position.getY()+diameter/4, position.getZ()-diameter/4);
-        Vector3 fourthNodeCentre = new Vector3(position.getX()+diameter/4, position.getY()+diameter/4, position.getZ()-diameter/4);
-        Vector3 fifthNodeCentre = new Vector3(position.getX()-diameter/4, position.getY()-diameter/4, position.getZ()+diameter/4);
-        Vector3 sixthNodeCentre = new Vector3(position.getX()+diameter/4, position.getY()-diameter/4, position.getZ()+diameter/4);
-        Vector3 seventhNodeCentre = new Vector3(position.getX()-diameter/4, position.getY()-diameter/4, position.getZ()-diameter/4);
-        Vector3 eigthNodeCentre = new Vector3(position.getX()+diameter/4, position.getY()-diameter/4, position.getZ()-diameter/4);
+
 
         if (body.insideOfBoundary(diameter/2, firstNodeCentre){
             if (firstNode == null){
